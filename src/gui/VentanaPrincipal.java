1a6a68c25359af9ba8c365b1ef783a56ed029e15
miguelraad28 <miguelraad2020@gmail.com>
2025-10-05 11:37:15 -0300

    Base window opening

--- src/gui/VentanaPrincipal.java
+++ src/gui/VentanaPrincipal.java
@@ -4,10 +4,54 @@
  */
 package gui;
 
+import javax.swing.JFrame;
+
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+
+
 /**
  *
  * @author Dell
  */
-public class VentanaPrincipal {
-    
+public class VentanaPrincipal extends JFrame {
+
+    public VentanaPrincipal() {
+        construirVentana();
+        construirEventos();
+    }
+
+    private void construirVentana() {
+        this.setTitle("Space Invaders - POO");
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setSize(800, 600);
+        this.setResizable(false);
+        this.setVisible(true);
+
+        JMenuBar menuBar = construirMenuBar();
+
+        this.setJMenuBar(menuBar);
+    }
+
+    private void construirEventos() {
+    }
+
+    private JMenuBar construirMenuBar() {
+        JMenuBar menuBar = new JMenuBar();
+
+        JMenu menuJuego = new JMenu("Juego");
+        JMenuItem nuevoJuego = new JMenuItem("Nuevo Juego");
+        //menuJuego.addActionListener(null);
+        menuJuego.add(nuevoJuego);
+
+        menuBar.add(menuJuego);
+        // Add oter menus.
+        /*menuBar.add(menuJuego);
+        menuBar.add(menuJuego);
+        menuBar.add(menuJuego);*/
+
+        return menuBar;
+    }
+
 }
--- src/spaceinvaders/SpaceInvaders.java
+++ src/spaceinvaders/SpaceInvaders.java
@@ -4,6 +4,8 @@
  */
 package spaceinvaders;
 
+import gui.VentanaPrincipal;
+
 /**
  *
  * @author Dell
@@ -14,7 +16,7 @@ public class SpaceInvaders {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        new VentanaPrincipal();
     }
     
 }
